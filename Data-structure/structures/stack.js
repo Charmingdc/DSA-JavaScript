6b1c28822be633a3c25f3330b284ae3589bc7bf7
Charmingdc <charmingdc002@gmail.com>
2025-03-06 17:06:52 +0100

    update

--- Data-structure/structures/stack.js
+++ Data-structure/structures/stack.js
@@ -1,36 +1,36 @@
 class myStack {
   constructor(storage, count) {
     this.storage = {},
-    this.count = 0
+      this.count = 0
   }
-  
-  
+
+
   push(value) {
     this.storage[this.count] = value; // add value to stack storage
     this.count++; // increment storage count
   }
-  
-  
+
+
   pop() {
     // return undefined is there's no value in the storage
     if (this.count === 0) return undefined;
-    
+
     this.count--; // decrement count
-    
+
     // set the last value in the stack as result value
     const result = this.storage[this.count];
-    
+
     // delete popped value from the storage
     delete this.storage[this.count];
 
     return result; // return popped value
   }
-  
+
   peek() {
     // return last value in the storage without deleting it
-    return this.storage[this.count-1];
+    return this.storage[this.count - 1];
   }
-  
+
   size() {
     // return count number
     return this.count;
@@ -40,9 +40,9 @@ class myStack {
 
 
 // const demo = new myStack();
-// demo.push('hello');
-// demo.push('world');
-// demo.push('test'); 
+// demo.push('string value');
+// demo.push(1);
+// demo.push(true)
 // console.log('Popped out:', demo.pop())
 // console.log('Peak:', demo.peek())
 // console.log('Count:', demo.size())
