1b4484cb3250297c8a4730c48769b5d910b4e7d5
Charmingdc <charmingdc002@gmail.com>
2025-03-14 23:27:52 +0100

    update binary.js

--- Data-structure/structures/binary.js
+++ Data-structure/structures/binary.js
@@ -1,1 +1,2 @@
 console.log('I feel sick doing this, but it"s not intentional, MTN network bring about it')
+console.log('I feel sick doing this, but it"s not intentional, MTN network bring about it')
